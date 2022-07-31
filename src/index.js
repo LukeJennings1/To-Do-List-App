9ce03f85b586318c252273c80371ef925ea6e4c1
Luke Jennings <lukejennings123@gmail.com>
2022-07-31 16:50:57 +0100

    added project functionality

--- src/index.js
+++ src/index.js
@@ -2,8 +2,19 @@
 const details = document.getElementById('info');
 const dueDate = document.getElementById('dateInput');
 const title = document.getElementById('titleInput');
-const getInfo = document.getElementById('submit-button')
-getInfo.addEventListener('click', () => {addToDo(), console.log(getInfo.value)});
+const getInfo = document.getElementById('submit-button');
+const createProject = document.getElementById('create-project');
+const toDoNotesGlobal = document.getElementById('toDoNotes');
+const tabHome = document.getElementById('Home');
+const sideMenu = document.getElementById('left-side-menu');
+
+
+getInfo.addEventListener('click', () => {addToDo()});
+createProject.addEventListener('click', () => {addProject()});
+tabHome.addEventListener('click', () => {homeTab()});
+
+
+
 // ^this takes the value of the title text field and adds console logs it
 // ^ this dates the value of the date text field
 // ^ this dates the value of the date text field
@@ -18,7 +29,8 @@ getInfo.addEventListener('click', () => {addToDo(), console.log(getInfo.value)})
 
         return (this.title + this.details + this.dueDate) }
 };
-let id = 0;
+let id = 0; 
+
 function addToDo() {
 const noteObject = new ToDo(title.value, details.value, dueDate.value);
 
@@ -48,6 +60,18 @@ delCreate.addEventListener('click', () => { divCreate.remove()})
 popUpBox.classList.remove('show')
 };
 
+let projectID = 0;           
+function addProject() {
+    toDoNotesGlobal.style.opacity = 0; // sets current content section as invisable
+    // remember to set this back to 1 when we go back to the home tab page. 
+    const createProjectDiv = document.createElement('div')
+    sideMenu.appendChild(createProjectDiv)
+    createProjectDiv.id = projectID++; // dynamic id assigned to each project div to allow deletion
+
+}
+function homeTab() {
+    toDoNotesGlobal.style.opacity = 1;
+}
 
 // add button animation popup
 const addButton = document.getElementById('add-Button')
@@ -61,7 +85,12 @@ delButton.addEventListener('click', () => {  popUpBox.classList.remove('show')})
 
 /* 1. Create and test constructor logic - TICK 
    2. Create basic website front in HTML and CSS - TICK 
-   3. Enable plus button to add a toDO note in seperate module -    
-   
+   3. Enable plus button to add a toDO note in seperate module -  TICK
+   4. Enable projects with different sets of toDo notes -   
+   ^ need to add a create Project button or create toDo buttonwhen + is pressed. 
+   If the project one is created we must be able to tab into it and create Todos. 
+   create a tab with the new project (this involves deleting the page and adding the new one
+    when the project is selected once created.)
+
    */
 
